4a843fe21d989c618c55b63099bffb95465c5029
Omar Abi Farraj <omar.7tech@gmail.com>
2026-05-14 01:13:03 +0300

    install

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
-    <>
-     
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {children}
+    </ThemeProvider>
   )
 }
 
