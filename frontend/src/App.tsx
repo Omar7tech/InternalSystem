ebf2fe5bd0e97d78e27db44140bf24772fe0bbaa
Omar Abi Farraj <omar.7tech@gmail.com>
2026-05-14 01:32:30 +0300

    install react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import type { ReactNode } from "react"
+import Nav from "@/components/nav"
 
 function App({ children }: { children?: ReactNode }) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Nav />
       {children}
     </ThemeProvider>
   )
